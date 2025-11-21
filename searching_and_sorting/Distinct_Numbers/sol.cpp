85a25dfa6fa22af6911c47e25a2452118fa62c63
Nilesh Pahari <nileshpahari@protonmail.com>
2025-11-21 23:52:03 +0530

    update state

---
+++ searching_and_sorting/Distinct_Numbers/sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                                                               \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(NULL);
+
+#define nl '\n'
+
+void solve() {
+  int n;
+  cin >> n;
+  set<int> s;
+  while (n--) {
+    int temp;
+    cin >> temp;
+    s.insert(temp);
+  }
+  cout << s.size();
+}
+
+int main() {
+  fastio();
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
