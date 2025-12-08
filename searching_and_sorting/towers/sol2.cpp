6cc1067988879897440cecf8cc340ce17a4b4f93
Nilesh Pahari <nileshpahari@protonmail.com>
2025-12-08 12:40:09 +0530

    update state

--- searching_and_sorting/towers/sol2.cpp
+++ searching_and_sorting/towers/sol2.cpp
@@ -19,8 +19,8 @@ void solve() {
 	cin >> x;
     auto up = s.upper_bound(x);
     if (up != s.end()) {
-		// s.erase(up); s.insert(x);
-		*up=x;
+		s.erase(up);
+		s.insert(x);
     } else {
       s.insert(x);
     }
@@ -36,40 +36,3 @@ int main() {
     solve();
   }
   return 0;
-// #include <algorithm>
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-//
-// #define fastio()                                                               \
-//   ios::sync_with_stdio(false);                                                 \
-//   cin.tie(NULL);
-//
-// #define nl '\n'
-//
-// void solve() {
-//   int n;
-//   cin >> n;
-//   vector<int> a;
-//   for (int i = 0; i < n; ++i) {
-//     int x;
-//     cin >> x;
-//     if (a.empty() or x >= a.back()) {
-// 		a.push_back(x);
-//     } else {
-// 		auto up = upper_bound(a.begin(), a.end(), x);
-// 		*up = x;
-// 	}
-//   }
-//   cout << (int)a.size();
-// }
-//
-// int main() {
-//   fastio();
-//   int t = 1;
-//   // cin >> t;
-//   while (t--) {
-//     solve();
-//   }
-//   return 0;
-// }
